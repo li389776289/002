c815167d9b8f789d9a294806edb39c3b0d4d5588
李金朋 <389776289@qq.com>
2018-12-16 18:54:33 +0800

    老哥稳!!!

---
+++ git_001/src/com/itheima/my_git.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class my_git {
+    public static void main(String[] args) {
+        System.out.println("我靠");
+        System.out.println("我靠");
+        System.out.println("我靠");
+        System.out.println("我靠");
+        System.out.println("你妹呀");
+    }
+}
